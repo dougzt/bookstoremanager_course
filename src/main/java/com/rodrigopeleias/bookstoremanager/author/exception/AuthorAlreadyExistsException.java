8b3988054562df2100877bb41350aeda6d5f1b43
dougzt <douglas_ads_santos@hotmail.com>
2020-11-30 17:07:12 -0300

    Developed author creation functionality

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/author/exception/AuthorAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.rodrigopeleias.bookstoremanager.author.exception;
+
+import javax.persistence.EntityExistsException;
+
+public class AuthorAlreadyExistsException extends EntityExistsException {
+    public AuthorAlreadyExistsException(String name) {
+        super(String.format("Author with name %s already exists!", name));
+    }
+}
--- src/test/java/com/rodrigopeleias/bookstoremanager/author/controller/AuthorControllerTest.java
+++ src/test/java/com/rodrigopeleias/bookstoremanager/author/controller/AuthorControllerTest.java
@@ -1,19 +1,35 @@
 package com.rodrigopeleias.bookstoremanager.author.controller;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.rodrigopeleias.bookstoremanager.author.builder.AuthorDTOBuilder;
+import com.rodrigopeleias.bookstoremanager.author.dto.AuthorDTO;
 import com.rodrigopeleias.bookstoremanager.author.service.AuthorService;
+import com.rodrigopeleias.bookstoremanager.utils.JsonConversionUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import static com.rodrigopeleias.bookstoremanager.utils.JsonConversionUtils.asJsonString;
+import static org.hamcrest.core.Is.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @ExtendWith(MockitoExtension.class)
 public class AuthorControllerTest {
+
+    private static final String AUTHORS_API_URL_PATH = "/api/v1/authors";
 
     @Mock
     private AuthorService authorService;
@@ -32,5 +48,31 @@ public class AuthorControllerTest {
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
                 .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenStatusCreatedShouldBeReturned() throws Exception {
+        AuthorDTO expectedCreatedAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+
+        Mockito.when(authorService.create(expectedCreatedAuthorDTO)).thenReturn(expectedCreatedAuthorDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(AUTHORS_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedCreatedAuthorDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(expectedCreatedAuthorDTO.getId().intValue())))
+                .andExpect(jsonPath("$.name", is(expectedCreatedAuthorDTO.getName())))
+                .andExpect(jsonPath("$.age", is(expectedCreatedAuthorDTO.getAge())));
+    }
+
+    @Test
+    void whenPOSTIsCalledWithoutRequiredFieldThenBadRequestShouldBeInformed() throws Exception {
+        AuthorDTO expectedCreatedAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+        expectedCreatedAuthorDTO.setName(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(AUTHORS_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(expectedCreatedAuthorDTO)))
+                .andExpect(status().isBadRequest());
     }
 }
