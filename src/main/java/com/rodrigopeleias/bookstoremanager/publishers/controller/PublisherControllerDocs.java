f4a1a32a0c04637e02e0f056956684755f52561d
dougzt <douglas_ads_santos@hotmail.com>
2020-11-30 20:28:37 -0300

    Developed structure classes for publisher module

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/publishers/controller/PublisherControllerDocs.java
@@ -0,0 +1,8 @@
+package com.rodrigopeleias.bookstoremanager.publishers.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Publishers management")
+public interface PublisherControllerDocs {
+
+}
