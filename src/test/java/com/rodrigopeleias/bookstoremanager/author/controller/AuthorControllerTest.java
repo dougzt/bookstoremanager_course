bfc79d55823f7fd9b45243710d378c7f03f5aad7
dougzt <douglas_ads_santos@hotmail.com>
2020-11-30 18:09:58 -0300

    developed list authors feature

--- src/test/java/com/rodrigopeleias/bookstoremanager/author/controller/AuthorControllerTest.java
+++ src/test/java/com/rodrigopeleias/bookstoremanager/author/controller/AuthorControllerTest.java
@@ -16,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import java.util.Collections;
+
 import static com.rodrigopeleias.bookstoremanager.utils.JsonConversionUtils.asJsonString;
 import static org.hamcrest.core.Is.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -83,5 +85,20 @@ public class AuthorControllerTest {
                 .andExpect(jsonPath("$.id", is(expectedFoundAuthorDTO.getId().intValue())))
                 .andExpect(jsonPath("$.name", is(expectedFoundAuthorDTO.getName())))
                 .andExpect(jsonPath("$.age", is(expectedFoundAuthorDTO.getAge())));
+    }
+
+
+    @Test
+    void whenGETListIsCalledThenStatusOKShouldBeReturned() throws Exception {
+        AuthorDTO expectedFoundAuthorDTO = authorDTOBuilder.buildAuthorDTO();
+
+        Mockito.when(authorService.findAll()).thenReturn(Collections.singletonList(expectedFoundAuthorDTO));
+
+        mockMvc.perform(get(AUTHORS_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(expectedFoundAuthorDTO.getId().intValue())))
+                .andExpect(jsonPath("$[0].name", is(expectedFoundAuthorDTO.getName())))
+                .andExpect(jsonPath("$[0].age", is(expectedFoundAuthorDTO.getAge())));
     }
 }
